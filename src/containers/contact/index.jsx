e68b5af27cf7aee746f8cac0cad01622bf3146ca
Anup Gupta <anup7.tech@gmail.com>
2024-10-27 12:20:47 -0700

    made some changes

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,14 +1,77 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
-import{BsInfoCircleFill} from "react-icons/bs"
-const Contact = ()=>{
-    return(
+import { BsInfoCircleFill } from "react-icons/bs"
+import { Animate } from "react-simple-animate";
+import "./styles.css"
+const Contact = () => {
+    return (
         <section className="contact">
-           <PageHeaderContent 
-           headerText="Contact me"
-           icon ={<BsInfoCircleFill size={40}/>}
-           />
+            <PageHeaderContent
+                headerText="Contact me"
+                icon={<BsInfoCircleFill size={40} />}
+            />
+
+
+            <div className="contact-content">
+                <Animate
+                    play
+                    duration={1}
+                    delay={0.3}
+                    start={{
+                        transform: "translateX(-200px)"
+                    }}
+
+                    end={{
+                        transform: "translateX(0px)"
+                    }}
+                >
+
+                    <h3 className="contact-content-header-text">Let's Talk</h3>
+
+                </Animate>
+
+                <Animate
+                    play
+                    duration={1}
+                    delay={0.3}
+                    start={{
+                        transform: "translateX(200px)"
+                    }}
+
+                    end={{
+                        transform: "translateX(0px)"
+                    }}
+                >
+
+                    <div className="contact-content-form">
+
+                        <div className="contact-content-form-controls-wrapper">
+                            <div>
+                                <input required name="name" className="inputName" type="text" />
+                                <label htmlFor="name" className="nameLabel">Name</label>
+                            </div>
+                            <div>
+                                <input required name="email" className="inputEmail" type="text" />
+                                <label htmlFor="email" className="emailLabel">Email</label>
+                            </div>
+                            <div>
+                                <textarea required name="description" className="inputDescription" type="text" rows="5" />
+                                <label htmlFor="description" className="descriptionLabel">Description</label>
+                            </div>
+                        </div>
+
+                        <button>Submit</button>
+
+                    </div>
+
+                </Animate>
+
+
+            </div>
+
         </section>
+
+
     )
 }
 
