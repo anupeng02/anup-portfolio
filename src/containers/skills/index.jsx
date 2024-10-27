3c1bc13f9866938277cbef849156d4e8bf1cf0a3
Anup Gupta <anup7.tech@gmail.com>
2024-10-27 04:20:47 -0700

    commited skills

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
-import {BsInfoCircleFill} from "react-icons/bs"
+import { BsInfoCircleFill } from "react-icons/bs"
+import { skillsData } from "./utils";
+import { Animate, AnimateKeyframes } from "react-simple-animate";
+import { Line } from "rc-progress";
+import "./styles.css"
 const Skills = () => {
     return (
         <section className="skills">
@@ -8,6 +12,63 @@ const Skills = () => {
                 headerText="Skills"
                 icon={<BsInfoCircleFill size={40} />}
             />
+
+            <div className="skillsContent-Wrapper">
+                {
+                    skillsData.map((item, i) => {
+                        return (
+                            <div key={i} className="skills-InnerContent-Wrapper" >
+                                <Animate
+                                    play
+                                    duration={1}
+                                    delay={0.3}
+                                    start={{
+                                        transform: 'translateX(-200px)'
+                                    }}
+                                    end={{
+                                        transform: 'translateX(0px)'
+                                    }}
+                                >
+
+                                    <h3 className="skills-InnerContent-Wrapper-categoryText">{item.label}</h3>
+
+                                    <div className="">
+                                        {item.data.map((skillsItem, j) => {
+                                            return (
+                                                <AnimateKeyframes
+                                                    play
+                                                    duration={1}
+                                                    delay={.3}
+                                                    keyframes={["opacity: 1", "opacity:0"]}
+                                                    iterationCount="1"
+                                                >
+                                                <div className="progressbar-wrapper" key={j}>
+                                                   <p>{skillsItem.skillName}</p>
+
+                                                   <Line
+                                                  percent={skillsItem.percentage} 
+                                                  strokeWidth="2"
+                                                  strokeColor="yellowgreen"
+                                                  trailWidth="2"
+                                                  strokeLinecap="square"
+                                                   />
+
+                                                </div>
+
+                                                </AnimateKeyframes>
+                                            )
+
+                                        })}
+
+                                    </div>
+                                </Animate>
+                            </div>
+                        )
+
+                    })
+                }
+
+            </div>
         </section>
     )
 }
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,70 @@
+export const skillsData = [
+
+    {
+        label: "Frontend",
+
+        data: [
+            {
+                skillsName: "HTML",
+                percentage: "95",
+            },
+            {
+                skillsName: "CSS",
+                percentage: "60",
+            },
+            {
+                skillsName: "JavaScript",
+                percentage: "80",
+            },
+            {
+                skillsName: "React.js",
+                percentage: "75",
+            },
+            {
+                skillsName: "Angular.js",
+                percentage: "35",
+            },
+        ]
+    },
+
+    {
+        label: "Backend",
+
+        data: [
+            {
+                skillsName: "Node.js",
+                percentage: "95",
+            },
+            {
+                skillsName: "Java",
+                percentage: "60",
+            },
+            {
+                skillsName: "Ruby",
+                percentage: "80",
+            },
+            
+        ]
+    },
+
+    {
+        label: "Database",
+
+        data: [
+            {
+                skillsName: "MYSQL",
+                percentage: "95",
+            },
+            {
+                skillsName: "MONGODB",
+                percentage: "60",
+            },
+            {
+                skillsName: "ORACLE",
+                percentage: "80",
+            },
+            
+        ]
+    },
+
+]
